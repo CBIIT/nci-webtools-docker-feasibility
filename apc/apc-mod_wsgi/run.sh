d840c6606d6582f5f060fe8e26f5f8bf0d0a2e25
park-brian <re.public@outlook.com>
2016-04-14 10:59:47 -0400

    clean up tmp directory

--- apc/apc-mod_wsgi/run.sh
+++ apc/apc-mod_wsgi/run.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-mkdir -p /test /test/git /test/deploy /test/wsgi
+mkdir -p /deploy/app /deploy/git /deploy/tmp
 
-cd /test/git
-git clone https://github.com/CBIIT/nci-webtools-dceg-age-period-cohort.git
-git clone https://github.com/CBIIT/nci-analysis-tools-web-presence.git
+git clone https://github.com/CBIIT/nci-webtools-dceg-age-period-cohort.git /deploy/tmp
+git clone https://github.com/CBIIT/nci-analysis-tools-web-presence.git /deploy/app
 
-cp -R nci-analysis-tools-web-presence/* /test/deploy/
-cp -R nci-webtools-dceg-age-period-cohort/apc /test/deploy/
+cp -R /deploy/tmp/apc /deploy/app/
+rm -rf /deploy/tmp
 
-echo 'from apc import app as application' > /test/deploy/apc/apc-deploy.wsgi
+echo 'from apc import app as application' > /deploy/app/apc/apc-deploy.wsgi
 
-mod_wsgi-express setup-server /test/deploy/apc/apc-deploy.wsgi --port 9040 --user apache --group apache --server-root=/test/wsgi/apc_test --document-root=/test/deploy --working-directory=/test/deploy/apc
+mod_wsgi-express setup-server /deploy/app/apc/apc-deploy.wsgi --port 9040 --user apache --group apache --server-root=/deploy --document-root=/deploy/app --working-directory=/deploy/app/apc
 
-chmod -R 777 /test
-chown -R apache:apache /test
-/test/wsgi/apc_test/apachectl start -DFOREGROUND 
+chmod -R 777 /deploy
+chown -R apache:apache /deploy
+/deploy/apachectl start -DFOREGROUND 
