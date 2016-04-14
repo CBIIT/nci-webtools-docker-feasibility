a560ec56aa9ee5b888a4d0dc42d0c321f8c90de0
park-brian <re.public@outlook.com>
2016-04-14 11:13:21 -0400

    shortened deployment script

--- apc/apc-mod_wsgi/run.sh
+++ apc/apc-mod_wsgi/run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-mkdir -p /deploy/app /deploy/git /deploy/tmp
+mkdir /deploy
 
 git clone https://github.com/CBIIT/nci-webtools-dceg-age-period-cohort.git /deploy/tmp
 git clone https://github.com/CBIIT/nci-analysis-tools-web-presence.git /deploy/app
 
-cp -R /deploy/tmp/apc /deploy/app/
+mv /deploy/tmp/apc /deploy/app/
 rm -rf /deploy/tmp
 
 echo 'from apc import app as application' > /deploy/app/apc/apc-deploy.wsgi
 
 mod_wsgi-express setup-server /deploy/app/apc/apc-deploy.wsgi --port 9040 --user apache --group apache --server-root=/deploy --document-root=/deploy/app --working-directory=/deploy/app/apc
 
-chmod -R 777 /deploy
 chown -R apache:apache /deploy
 /deploy/apachectl start -DFOREGROUND 
