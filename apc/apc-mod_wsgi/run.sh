bf359c89c3f771865fd6eb56666552cba3eed0ce
Brian Park <park-brian@users.noreply.github.com>
2016-04-14 08:53:56 -0400

    updated deployment script
    set permissions and start apache in foreground

--- apc/apc-mod_wsgi/run.sh
+++ apc/apc-mod_wsgi/run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-mkdir git deploy wsgi
+mkdir -p /test/git /test/deploy /test/wsgi
 
-cd git
+cd /test/git
 git clone https://github.com/CBIIT/nci-webtools-dceg-age-period-cohort.git
 git clone https://github.com/CBIIT/nci-analysis-tools-web-presence.git
 
-cp -R nci-analysis-tools-web-presence/* ../deploy/
-cp -R nci-webtools-dceg-age-period-cohort/apc ../deploy/
-cd ..
+cp -R nci-analysis-tools-web-presence/* /test/deploy/
+cp -R nci-webtools-dceg-age-period-cohort/apc /test/deploy/
 
-echo 'from apc import app as application' > deploy/apc/apc-deploy.wsgi
+echo 'from apc import app as application' > /test/deploy/apc/apc-deploy.wsgi
 
-mod_wsgi-express setup-server deploy/apc/apc-deploy.wsgi --port 9040 --user apache --group apache --server-root=wsgi/apc_test --document-root=deploy --working-directory=deploy/apc
+mod_wsgi-express setup-server /test/deploy/apc/apc-deploy.wsgi --port 9040 --user apache --group apache --server-root=/test/wsgi/apc_test --document-root=/test/deploy --working-directory=/test/deploy/apc
 
-~/wsgi/apc_test/apachectl start
+chmod -R 777 /test
+chown -R apache:apache /test
+/test/wsgi/apc_test/apachectl start -DFOREGROUND 
