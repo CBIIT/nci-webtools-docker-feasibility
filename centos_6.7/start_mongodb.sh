751e2d59ecabbfd35118dfdb36d941ab3a10ab5f
Brian Park <park-brian@users.noreply.github.com>
2016-05-12 15:54:09 -0400

    using host network for ldlink

--- centos_6.7/start_mongodb.sh
+++ centos_6.7/start_mongodb.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker network create -d bridge analysistools-net
-docker run -dp 27017 -v /data:/data/ldlink/mongo/data --net=analysistools-net cbiitss:mongodb
+docker run -dp 27017 -v /data:/data/ldlink/mongo/data --net host cbiitss:mongodb
